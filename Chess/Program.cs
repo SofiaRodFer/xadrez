81b1b478d285af85bc6b233bb5dd36757ea13081
Sofia <sofiarodrigues45@gmail.com>
2021-10-02 17:57:43 -0300

    Controlando as peças capturadas

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -12,15 +12,12 @@ namespace Chess
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                while(!partida.terminada)
+                while (!partida.terminada)
                 {
                     try
                     {
                         Console.Clear();
-                        Tela.imprimirTabuleiro(partida.tab);
-                        Console.WriteLine();
-                        Console.WriteLine("Turno: " + partida.turno);
-                        Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                        Tela.imprimirPartida(partida);
 
                         Console.WriteLine();
                         Console.Write("Origem: ");
