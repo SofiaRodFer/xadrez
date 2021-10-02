a891eb544e66ed3b4161f6015728a673674fb92e
Sofia <sofiarodrigues45@gmail.com>
2021-10-02 15:59:49 -0300

    Fazendo os primeiros testes de movimentação das peças

--- Chess/tabuleiro/Peca.cs
+++ Chess/tabuleiro/Peca.cs
@@ -17,5 +17,10 @@
             this.cor = cor;
             this.qteMovimentos = 0;
         }
+
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
     }
 }
